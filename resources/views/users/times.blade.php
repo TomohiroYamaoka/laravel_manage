6cc032fac4f31657e5dcd67df49e5ea068c7b3c8
山岡智裕 <>
2020-08-24 12:19:49 +0900

    ログアウト・ログインのバグ修正

--- resources/views/users/times.blade.php
+++ resources/views/users/times.blade.php
@@ -1,7 +1,5 @@
 @extends('app')
 
-@section('title', '打刻')
-
 @section('content')
 <!--打刻終了ボタン-->
 <div class="row">
@@ -10,8 +8,7 @@
       <div class="card-body">
         <h5 class="card-title">打刻する際はこちら</h5>
         <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">打刻</a>
-        <form action="{{ route('timestamp/punchin') }}" method="POST">
+        <form action="" method="POST">
         @csrf
         @method('POST')
         <button type="submit" class="btn btn-primary">打刻</button>
@@ -25,7 +22,7 @@
       <div class="card-body">
         <h5 class="card-title">仕事が終わる際はこちら</h5>
         <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <form action="{{ route('timestamp/punchout') }}" method="POST">
+        <form action="" method="POST">
         @csrf
         @method('POST')
         <button type="submit" class="btn btn-primary">終了</button>
@@ -35,29 +32,3 @@
   </div>
 </div>
 
-
-<h2>打刻・終了履歴</h2>
-<!--打刻した時間を-->
-<table class="table table-bordered">
-   <!--項目-->
-   <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">日付</th>
-      <th scope="col">打刻時間</th>
-      <th scope="col">終了時刻</th>
-    </tr>
-　　</thead>
-
-  <tbody>
-    @foreach($timestamps as $timestamp)
-    <!--1行を繰り返す-->
-    <tr>
-      <th scope="row"></th>
-      <td>12/15</td>
-      <td>{{ $timestamp->punchIn }}</td>
-      <td>{{ $timestamp->punchOut }}</td>
-    </tr>
-  </tbody>
-</table>
-@endsection
